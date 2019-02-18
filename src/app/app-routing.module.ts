9339ce644bf17f623b203a6da17e09af81b9f118
scorpio611 <evilmonter666@gmail.com>
2019-02-18 21:16:24 +0700

    [Thực hành] Ứng dụng blog [Bài tập] Ứng dụng todo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {BlogEditComponent} from './blog-edit/blog-edit.component';
+import {BlogDetailComponent} from './blog-detail/blog-detail.component';
+import {BlogComponent} from './blog/blog.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'blog',
+  component: BlogComponent
+}, {
+  path: 'blog/:id',
+  component: BlogDetailComponent
+}, {
+  path: 'blog/:id/edit',
+  component: BlogEditComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {TodoComponent} from './todo/todo.component';
+import {TokenInterceptor} from './token.interceptor';
+import {BlogComponent} from './blog/blog.component';
+import {BlogEditComponent} from './blog-edit/blog-edit.component';
+import {BlogDetailComponent} from './blog-detail/blog-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    BlogComponent,
+    BlogEditComponent,
+    BlogDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptor,
+    multi: true
+  },
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
